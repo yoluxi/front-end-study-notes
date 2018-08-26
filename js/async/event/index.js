06e42506498a78f677bea4d79baf7a8cb5ddda9a
黎旭 <146995@syswin.com>
2018-08-26 15:14:39 +0800

    addsomething

---
+++ js/async/event/index.js
@@ -0,0 +1,28 @@
+
+// 线程的阻塞
+var start = new Date();
+setTimeout(function() {
+    var end = new Date();
+    console.log("time elapsed", end - start, 'ms')   // 1002ms
+}, 500)
+
+// while(new Date - start < 1000) {}
+// 疑问的点
+// 异步回调什么时候放进主进程的？
+
+
+// 异步的计时函数
+var fireCount = 0;
+var startA = new Date();
+var timer = setInterval(function(){
+    if (new Date - start > 1000) {
+        clearInterval(timer)
+        console.log(fireCount) // 252
+        return
+    }
+    fireCount++
+}, 0)
+
+
+
+
